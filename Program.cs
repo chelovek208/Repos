3dcac0ce7124623ae08b3c9f7ee398b04042f076
User403 <aalekseev648@outlook.com>
2019-11-02 17:39:44 +0500

    2

--- Program.cs
+++ Program.cs
@@ -1,31 +1,43 @@
-﻿using System;
-
-namespace ConsoleApp1
+﻿namespace ConsoleApp1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int First, Second;
-            int[] Array = new int[5];
+            Console.WriteLine("Введите количество элементов");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int[] Nums = new int[N];
+            for (int i = 0; i < Nums.Length;)
+            {
+                Console.WriteLine("Введите элемент " + (i + 1));
+                Nums[i] = Convert.ToInt32(Console.ReadLine());
+                i++;
+            }
+            int Num = 0;
 
-            for (int i = 0; i < Array.Length;)
+            Console.WriteLine("Введите количество максимальных значений элементов");
+            int M = Convert.ToInt32(Console.ReadLine());
+            int[] MaxNums = new int[M + 1];
+            MaxNums[0] = 2147483647;
+
+            for (int i = 1; i < MaxNums.Length; i++)
             {
-                Array[i] = Convert.ToInt32(Console.ReadLine());
-                i++;
-            }
-            First = Array[0];
-            Second = -2147483648;
-            for (int i = 0; i < Array.Length;)
-            {
-                if (First < Array[i])
+                for (int j = 0; j < Nums.Length; j++)
                 {
-                    Second = First;
-                    First = Array[i];
+                    if (Num < Nums[j] && Nums[j] < MaxNums[i - 1])
+                    { Num = Nums[j]; }
                 }
-                i++;
+                MaxNums[i] = Num;
+                Num = 0;
             }
-            Console.WriteLine("First - " + First + " Second - " + Second);
+
+
+            // не обязательно
+            Console.WriteLine();
+            for (int i = 1; i < MaxNums.Length; i++)
+            {
+                Console.WriteLine(MaxNums[i]);
+            }
         }
 
     }
