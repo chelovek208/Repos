c52348eae14b196c75e064f1b58dedbc2bfa2104
User403 <aalekseev648@outlook.com>
2019-11-02 12:56:21 +0500

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int First, Second;
+            int[] Array = new int[5];
+
+            for (int i = 0; i < Array.Length;)
+            {
+                Array[i] = Convert.ToInt32(Console.ReadLine());
+                i++;
+            }
+            First = Array[0];
+            Second = -2147483648;
+            for (int i = 0; i < Array.Length;)
+            {
+                if (First < Array[i])
+                {
+                    Second = First;
+                    First = Array[i];
+                }
+                i++;
+            }
+            Console.WriteLine("First - " + First + " Second - " + Second);
+        }
+
+    }
+}
